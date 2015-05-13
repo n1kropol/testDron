75c6053ea0a4cd38f13854eb2be7e486d7962785
n1kropol <n1kropol@yandex.ua>
2015-05-13 16:33:41 +0300

    Update AngelTitleTest.java

--- src/AngelTitleTest.java
+++ src/AngelTitleTest.java
@@ -1,3 +1,9 @@
+/**
+ * Created by n1kropol on 13.05.2015.
+ */
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
@@ -5,38 +11,15 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.openqa.selenium.*;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 
-import java.util.ArrayList;
-import java.util.List;
-/**
- * Created by n1kropol on 29.04.2015.
- */
-@RunWith(JUnit4.class)
-public class AngelTitleTest {
-    public static WebDriver driver;
-    @Before
-    public void doBefore(){
-//        TestHelperPassw.init();
+public class forDron {
+@Test
+    public void test1(){
+        WebDriver driver;
         System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
         driver = new ChromeDriver();
         driver.get("http://angel.net/~nic/passwd.current.html");
-    }
-
-    @Test
-    public void test1(){
-        //TestHelperPassw.driver.findElement(By.name("master"));
-        //TestHelperPassw.driver.findElement(By.name("site"));
-        //TestHelperPassw.driver.findElement(By.xpath("html/body/form/table/tbody/tr[3]/td/input")).click();
-        //Assert.assertEquals("BaefBs8/Z/cm2@1a", TestHelperPassw.driver.findElement(By.name("password")).getAttribute("value"));
-        Assert.assertEquals(1,1);
-    }
-
-    @After
-    public void clean(){
-        //TestHelperPassw.driver.quit();
         driver.quit();
     }
+    
 }
-
